3ca637ebf0d4a7ad6bf4fc5a0b0dbc7b1f40b1c8
Bryan Quintana <bryandise@gmail.com>
2024-02-18 16:25:26 -0300

    .

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -3,8 +3,8 @@ const DB_NAME = "customer-invoices";
 const DB_URI = process.env.MONGODB_URI || `mongodb://127.0.0.1/${DB_NAME}`;
 
 const mongooseConfig = ()=> {
-    console.log("DB_URI: " + DB_URI);
     mongoose.set('strictQuery', false);
+    console.log("DB_URI: " + DB_URI);
     mongoose.connect(DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
