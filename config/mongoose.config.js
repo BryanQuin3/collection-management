e869e75ba000f7ac62499cf38f7a035773da5f6e
Bryan Quintana <bryandise@gmail.com>
2024-02-18 16:18:42 -0300

    Remove console.log statement in mongoose.config.js

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -3,7 +3,6 @@ const DB_NAME = "customer-invoices";
 const DB_URI = process.env.MONGODB_URI || `mongodb://127.0.0.1/${DB_NAME}`;
 
 const mongooseConfig = ()=> {
-    console.log("DB_URI: " + DB_URI);
     mongoose.set('strictQuery', false);
     mongoose.connect(DB_URI, {
     useNewUrlParser: true,
