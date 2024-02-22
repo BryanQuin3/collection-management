6b388386269fe87fe3c60de96297d9dccaf5c2b7
Bryan Quintana <bryandise@gmail.com>
2024-02-22 09:49:18 -0300

    Add password validation in createUser function

--- controllers/user.js
+++ controllers/user.js
@@ -10,10 +10,13 @@ module.exports.createUser = async (req, res) => {
         if (userExists) {
             return res.status(400).json({ message: "User already exists" });
         }
+        if (req.body.password !== req.body.confirmPassword) {
+            return res.status(400).json({ message: "Passwords do not match" });
+        }
         const newUser = await User.create(req.body);
         res.status(200).json(newUser);
     } catch (error) {
-        res.status(500).json(error);
+        res.status(500).json({ message: "Server error" });
     }
 }
 
