0ae8f05c9576eb23199690638c9666b5945cb4db
Bryan Quintana <bryandise@gmail.com>
2024-03-19 10:43:37 -0300

    Refactor image file validation in createCustomer function

--- controllers/customer.js
+++ controllers/customer.js
@@ -9,20 +9,6 @@ module.exports.createCustomer = async (req, res) => {
         let customer = new Customer({...req.body});
         if(req.file && req.file.originalname !== 'undefined'){
             const { filename } = req.file;
-            // verificar si el archivo es una imagen .png, .jpg, .jpeg .avif o .webp
-            const ext = (path.extname(filename)).toLowerCase();
-            const filetypes = /png|jpg|jpeg|avif|webp/;
-            const mimetype = filetypes.test(req.file.mimetype);
-            const extname = filetypes.test(ext);
-            if (!mimetype && !extname) {
-                console.log('File type not allowed');
-                return res.status(400).json({ message: "File type not allowed" });
-            }
-            // verificar si el archivo es mayor a 2MB
-            if (req.file.size >= 1000000) {
-                console.log('The image must be less than 1MB');
-                return res.status(400).json({ message: "The image must be less than 1MB" });
-            }
             customer.setImageUrl(filename);
         }
         else{
