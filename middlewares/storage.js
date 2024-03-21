ad4866995d0dc7fcd195a5a20adca3a6472167cb
Bryan Quintana <bryandise@gmail.com>
2024-03-21 10:42:58 -0300

    Add fs module for file system operations

--- middlewares/storage.js
+++ middlewares/storage.js
@@ -1,6 +1,7 @@
 const multer = require("multer");
 const path = require("path");
 const sharp = require("sharp");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
