e45515204543a4408052e9ac12c46f7b390db636
Bryan Quintana <bryandise@gmail.com>
2024-03-21 09:52:49 -0300

    Add image resizing functionality to customer route

--- middlewares/storage.js
+++ middlewares/storage.js
@@ -1,5 +1,7 @@
 const multer = require("multer");
 const path = require("path");
+const sharp = require("sharp");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -13,4 +15,24 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-module.exports = upload;
+// Middleware para redimensionar la imagen
+const resizeImage = async (req, res, next) => {
+    if (!req.file) return next();
+    const originalFilePath = req.file.path;
+    const newFilePath = originalFilePath.replace(path.extname(originalFilePath), '-resized' + path.extname(originalFilePath));
+    try {
+        await sharp(originalFilePath)
+            .resize(32, 32)
+            .toFile(newFilePath);
+        // Eliminar el archivo original después de redimensionarlo
+        fs.unlinkSync(originalFilePath);
+        // Establecer la ruta del archivo redimensionado en el objeto req
+        req.file.path = newFilePath;
+        next();
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({ error: "Error resizing the image" });
+    }
+};
+
+module.exports = { upload, resizeImage };
--- routes/customer.js
+++ routes/customer.js
@@ -1,8 +1,8 @@
 const CustomerController = require('../controllers/customer');
-const upload = require('../middlewares/storage');
+const { upload, resizeImage } = require('../middlewares/storage');
 
 module.exports = (app) => {
-    app.post('/customers',upload.single('image_url') ,CustomerController.createCustomer);
+    app.post('/customers',upload.single('image_url'),resizeImage ,CustomerController.createCustomer);
     app.get('/customers', CustomerController.getAllCustomers);
     app.get('/customers/count', CustomerController.getCustomerCount);
     app.get('/customers/:id', CustomerController.getCustomerById);
